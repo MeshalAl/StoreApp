f6c83f05f81fc8afd52f14d2f0dfdb1f3a970e29
Meshal <meshalalanazinsa@gmail.com>
2023-02-04 22:57:29 +0300

    added cart functions

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Cart } from 'src/app/models/Cart';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductListComponent } from '../product-list/product-list.component';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
 
+
+export class CartComponent {
+  cartProducts: Cart[] = [];
+
+  constructor(private CartService: CartService) {};
+
+  ngOnInit(){
+    this.cartProducts = this.CartService.getCartProducts();
+  }
+
+  
 }
