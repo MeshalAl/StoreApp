a4e64d247103ef68f321995236389adf532e1adf
Meshal <meshalalanazinsa@gmail.com>
2023-02-13 01:43:27 +0300

    added confirmation component

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { OrderService } from 'src/app/services/order.service';
+import { Order } from 'src/app/models/Order';
+import { Router } from '@angular/router';
+import { core } from '@angular/compiler';
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent {
+  customer: string = '';
+  total: number = 0;
 
+  constructor(private OrderService: OrderService, private router: Router) {}
+  
+  ngOnInit(): void {
+    const order = this.OrderService.getOrder();
+    this.customer = order.customer;
+    this.total = order.total;
+
+    if (order.total == 0){
+      this.router.navigate(['/'])
+    }
+  }
+  
 }
