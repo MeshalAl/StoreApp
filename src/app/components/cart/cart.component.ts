a05e3ac9835bd5b8323d66779d1e47997c52c76f
Meshal <meshalalanazinsa@gmail.com>
2023-02-13 01:42:48 +0300

    added remaining cart functions

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { Cart } from 'src/app/models/Cart';
 import { CartService } from 'src/app/services/cart.service';
 import { ProductListComponent } from '../product-list/product-list.component';
-
+import { Order } from 'src/app/models/Order';
+import { OrderService } from 'src/app/services/order.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -14,21 +16,41 @@ export class CartComponent {
   cartProducts: Cart[] = [];
   total: number = 0;
 
-  constructor(private CartService: CartService) {};
+  constructor(private CartService: CartService, private OrderService: OrderService, private router: Router) {};
 
   ngOnInit(){
     this.cartProducts = this.CartService.getCartProducts();
     this.calculateTotal();
   }
 
-  changedQuantity(event: any) {
-   // calculate total 
+  changedQuantity(event: any, i: number) {
+    if(event == 0){
+      this.cartProducts = this.CartService.removedFromCart(i);
+      this.calculateTotal();
+      alert("Removed from cart!");
+    } else {
+      this.cartProducts[i].quantity = event;
+      this.calculateTotal();
+    }
+
   }
 
   calculateTotal() {
+    this.total = 0
     this.cartProducts.forEach( (i) => {
       this.total += i.quantity * i.product.price;
     })
   }
-  
+  submit(form: any) {
+    const order: Order = {
+      customer: form.value.name,
+      address: form.value.address,
+      total: this.total,
+      orderCart: this.cartProducts
+    };
+    
+    this.OrderService.submitOrder(order);
+    this.router.navigate(['confirmation']);
+    this.CartService.clearCart();
+  }
 }
